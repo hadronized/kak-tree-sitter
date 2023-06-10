d2d9761f309d6204a8f4480dc23f10558a165e29
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2023-06-10 15:14:36 +0200

    Hand-craft markdown queries.

---
+++ runtime/queries/markdown/injections.scm
@@ -0,0 +1,24 @@
+; kak-tree-sitter notes: taken from helix-editor/helix
+
+; From nvim-treesitter/nvim-treesitter
+
+(fenced_code_block
+  (code_fence_content) @injection.shebang @injection.content
+  (#set! injection.include-unnamed-children))
+
+(fenced_code_block
+  (info_string
+    (language) @injection.language)
+  (code_fence_content) @injection.content (#set! injection.include-unnamed-children))
+
+((html_block) @injection.content
+ (#set! injection.language "html")
+ (#set! injection.include-unnamed-children)
+ (#set! injection.combined))
+
+((pipe_table_cell) @injection.content (#set! injection.language "markdown.inline") (#set! injection.include-children))
+
+((minus_metadata) @injection.content (#set! injection.language "yaml") (#set! injection.include-children))
+((plus_metadata) @injection.content (#set! injection.language "toml") (#set! injection.include-children))
+
+((inline) @injection.content (#set! injection.language "markdown.inline") (#set! injection.include-children))
