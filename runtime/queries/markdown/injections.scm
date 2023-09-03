2ab17530bf7d44d806b5457c882f1c3672097382
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2023-09-03 12:21:36 +0200

    Fix Markdown injections.

--- runtime/queries/markdown/injections.scm
+++ runtime/queries/markdown/injections.scm
@@ -4,16 +4,16 @@
 
 (fenced_code_block
   (code_fence_content) @injection.shebang @injection.content
-  (#set! injection.include-unnamed-children))
+  (#set! injection.include-children))
 
 (fenced_code_block
   (info_string
     (language) @injection.language)
-  (code_fence_content) @injection.content (#set! injection.include-unnamed-children))
+  (code_fence_content) @injection.content (#set! injection.include-children))
 
 ((html_block) @injection.content
  (#set! injection.language "html")
- (#set! injection.include-unnamed-children)
+ (#set! injection.include-children)
  (#set! injection.combined))
 
 ((pipe_table_cell) @injection.content (#set! injection.language "markdown.inline") (#set! injection.include-children))
