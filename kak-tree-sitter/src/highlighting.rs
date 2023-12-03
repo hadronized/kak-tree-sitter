hadronized/kak-tree-sitter

commit 7db85a4b4e394750e633ceea8e390e052c61dac0

Starting to fix #187.
I don’t think the `- 1` is actually needed, because we get events and we want to reach a given byte, since we are bound
by the parser emitting events (actually, `tree-sitter-highlight`).

kak-tree-sitter/src/highlighting.rs
@@ -123,7 +123,7 @@ impl KakHighlightRange {
           let line_start = mapper.line();
           let col_byte_start = mapper.col_byte();
 
-          mapper.advance(end - 1);
+          mapper.advance(end);
           let line_end = mapper.line();
           let col_byte_end = mapper.col_byte();
 
@@ -324,5 +324,26 @@ mod tests {
     mapper.advance(1);
     assert_eq!(mapper.line(), 1);
     assert_eq!(mapper.col_byte(), 2);
+  }
+
+  #[test]
+  fn newline_mapper() {
+    let source = "×\na"; // 2 bytes, 1 byte, 1 byte
+    let mut mapper = ByteLineColMapper::new(source.graphemes(true));
+
+    assert_eq!(mapper.line(), 1);
+    assert_eq!(mapper.col_byte(), 0);
+
+    mapper.advance(1);
+    assert_eq!(mapper.line(), 1);
+    assert_eq!(mapper.col_byte(), 2);
+
+    mapper.advance(2);
+    assert_eq!(mapper.line(), 1);
+    assert_eq!(mapper.col_byte(), 2);
+
+    mapper.advance(3);
+    assert_eq!(mapper.line(), 2);
+    assert_eq!(mapper.col_byte(), 0);
   }
 }

commit 974dc39e1919d420e2d218a4ac19f93ab716dde5

Fix regression introduced in 7db85a4b4e394750e633ceea8e390e052c61dac0.
We need more time for this `- 1` thing.

kak-tree-sitter-config/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "kak-tree-sitter-config"
-version = "0.4.0-dev"
+version = "0.4.0"
 license = "BSD-3-Clause"
 authors = ["Dimitri Sabadie <dimitri.sabadie@gmail.com>"]
 description = "Server between Kakoune and tree-sitter; configuration layer"

kak-tree-sitter/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "kak-tree-sitter"
-version = "0.5.1-dev"
+version = "0.5.2"
 license = "BSD-3-Clause"
 authors = ["Dimitri Sabadie <dimitri.sabadie@gmail.com>"]
 description = "Server between Kakoune and tree-sitter"
@@ -18,7 +18,7 @@ ctrlc = "3.4.1"
 daemonize = "0.5.0"
 dirs = "5.0.0"
 itertools = "0.12.0"
-kak-tree-sitter-config = { version = "0.4.0-dev", path = "../kak-tree-sitter-config" }
+kak-tree-sitter-config = { version = "0.4.0", path = "../kak-tree-sitter-config" }
 libc = "0.2.150"
 libloading = "0.8.1"
 log = "0.4.20"

kak-tree-sitter/src/highlighting.rs
@@ -123,7 +123,7 @@ impl KakHighlightRange {
           let line_start = mapper.line();
           let col_byte_start = mapper.col_byte();
 
-          mapper.advance(end);
+          mapper.advance(end - 1);
           let line_end = mapper.line();
           let col_byte_end = mapper.col_byte();
 

ktsctl/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "ktsctl"
-version = "0.3.3-dev"
+version = "0.3.3"
 license = "BSD-3-Clause"
 authors = ["Dimitri Sabadie <dimitri.sabadie@gmail.com>"]
 description = "CLI controler of kak-tree-sitter"
@@ -16,6 +16,5 @@ rust-version = "1.65.0"
 clap = { version = "4.4.8", features = ["derive"] }
 colored = "2.0.4"
 dirs = "5.0.1"
-#kak-tree-sitter-config = { version = ">=0.3.0, <0.5", path = "../kak-tree-sitter-config" }
-kak-tree-sitter-config = { version = "0.4.0-dev", path = "../kak-tree-sitter-config" }
+kak-tree-sitter-config = { version = ">=0.3.0, <0.5", path = "../kak-tree-sitter-config" }
 thiserror = "1.0.50"