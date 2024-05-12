862f6cd58b38d9d1b4abfb722b4a4b25bdae2586
zetashift <rskaraya@gmail.com>
2024-05-12 23:24:58 +0200

    add astro highlights

---
+++ runtime/queries/astro/README.md
@@ -0,0 +1,3 @@
+All the files in this directory were taken and adapted from
+[Helix Astro queries](https://github.com/helix-editor/helix/tree/master/runtime/queries/astro) and [Helix HTML queries](https://github.com/helix-editor/helix/tree/master/runtime/queries/html).
+They are thus licensed under the [Mozilla Public License 2.0](https://github.com/helix-editor/helix/blob/master/LICENSE).
---
+++ runtime/queries/astro/injections.scm
@@ -0,0 +1,18 @@
+((comment) @injection.content
+ (#set! injection.language "comment"))
+
+((script_element
+  (raw_text) @injection.content)
+ (#set! injection.language "javascript"))
+
+((style_element
+  (raw_text) @injection.content)
+ (#set! injection.language "css"))
+
+((frontmatter
+	(raw_text) @injection.content)
+ (#set! injection.language "typescript"))
+
+((interpolation
+	(raw_text) @injection.content)
+ (#set! injection.language "tsx"))
